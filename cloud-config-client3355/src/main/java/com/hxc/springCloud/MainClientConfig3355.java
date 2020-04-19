7eff2fb4c889c4d3e39d7612bc4149277811a318
hxc <3129571932@qq.com>
2020-04-19 13:06:59 +0800

    client

---
+++ cloud-config-client3355/src/main/java/com/hxc/springCloud/MainClientConfig3355.java
@@ -0,0 +1,16 @@
+package com.hxc.springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import sun.applet.Main;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MainClientConfig3355 {
+    public static void main(String[] args) {
+
+        SpringApplication.run(MainClientConfig3355.class,args);
+
+    }
+}

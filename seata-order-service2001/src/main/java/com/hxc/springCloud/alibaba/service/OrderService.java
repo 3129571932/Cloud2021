ca37e589e76ddc14e4e0f1343d23c4744a7d5d15
hxc <3129571932>
2020-05-04 21:31:57 +0800

    123

---
+++ seata-order-service2001/src/main/java/com/hxc/springCloud/alibaba/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.hxc.springCloud.alibaba.service;
+
+import com.hxc.springCloud.alibaba.domain.Order;
+
+public interface OrderService {
+    void create(Order order);
+}
---
+++ seata-order-service2001/src/main/java/com/hxc/springCloud/alibaba/service/StorageService.java
@@ -0,0 +1,13 @@
+package com.hxc.springCloud.alibaba.service;
+
+import com.hxc.springCloud.alibaba.domain.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "seata-storage-service")
+public interface StorageService
+{
+    @PostMapping(value = "/storage/decrease")
+    CommonResult decrease(@RequestParam("productId") Long productId, @RequestParam("count") Integer count);
+}
